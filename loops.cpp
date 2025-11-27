cc6c91fb1492c91fd6ddf1b4b44014670b75c4bf
Aman Coder <115057435+aman-coder28@users.noreply.github.com>
2025-11-27 10:03:16 +0300

    Update loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -11,13 +11,15 @@ int main() {
 
   int n = 10;
 
-  // while (n > 0) {
-  //   printf("%d", n);
-  //   n--;
-  // }
+  while (n > 0) {
+     printf("%d", n);
+
+     n--;
+  }
 
   do {
     printf("%d \n", n);
+
     n--;
   } while (n > 0);
 
